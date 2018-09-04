277dc9fb4bc1e23eefa4d3aa662aab07a8c9b2d5
Maksim Kostromin <daggerok@gmail.com>
2018-09-04 16:42:21 +0300

    Update some versions, readme and other useful stuff...

--- apps/auth-server/src/main/java/com/github/daggerok/oauth2authserver/app/AuthenticationManagerConfig.java
+++ apps/auth-server/src/main/java/com/github/daggerok/oauth2authserver/app/AuthenticationManagerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //tag::authentication-manager-config[]
@@ -25,6 +26,30 @@ public class AuthenticationManagerConfig extends WebSecurityConfigurerAdapter {
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    //@formatter:off
+    http
+        .csrf()
+          .disable()
+        .headers()
+          .frameOptions()
+            .sameOrigin()
+            .and()
+        .authorizeRequests()
+          .antMatchers("/login", "/h2-console/**").permitAll()
+        .anyRequest()
+          .fullyAuthenticated()
+        //.and()
+        //  .logout()
+        //    .clearAuthentication(true)
+        //    .invalidateHttpSession(true)
+        //    .deleteCookies("JSESSIONID", "JSESSION", "SESSIONID", "SESSION")
+        //    .permitAll(true)
+    ;
+    //@formatter:on
   }
 }
 //end::authentication-manager-config[]
--- apps/auth-server/src/main/java/com/github/daggerok/oauth2authserver/app/JdbcUserDetailsService.java
+++ apps/auth-server/src/main/java/com/github/daggerok/oauth2authserver/app/JdbcUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.github.daggerok.oauth2authserver.app;
 
+import io.vavr.collection.HashMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.User;
@@ -9,28 +10,49 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+import static java.lang.String.format;
+
 //tag::jdbc-user-details-service[]
 @Service
 @RequiredArgsConstructor
 public class JdbcUserDetailsService implements UserDetailsService {
 
+  // in memory applications credentials database configuration:
+  private static final Map<String, String> db = HashMap.of(
+      "usr", "pwd"
+  ).toJavaMap();
+
   final PasswordEncoder encoder;
   final JdbcTemplate jdbcTemplate;
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    //@formatter:off
+  @PostConstruct
+  public void init() {
+    db.forEach(this::mapToUser);
+  }
+
+  private UserDetails mapToUser(final String username, final String password) {
     return User
-        .withUsername("usr")
-        //.password("pwd")
-        .password(null == encoder ? "{noop}pwd" : encoder.encode("pwd"))
+        .withUsername(username)
+        .password(null == encoder ? format("{noop}%s", password) : encoder.encode(password))
         .disabled(false)
         .accountExpired(false)
         .accountLocked(false)
         .credentialsExpired(false)
         .authorities("USER", "ADMIN", "ROLE_USER", "ROLE_ADMIN")
         .build();
-    //@formatter:on
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    final Map.Entry<String, String> pair = db.entrySet()
+                                             .parallelStream()
+                                             .filter(e -> e.getKey().equals(username))
+                                             .findFirst()
+                                             .orElseThrow(() -> new UsernameNotFoundException(username));
+    return mapToUser(pair.getKey(), pair.getValue());
   }
 }
 //end::jdbc-user-details-service[]
